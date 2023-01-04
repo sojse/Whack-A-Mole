967353a76df2cf53137c21bbc7cdcc4f7cb7e246
Josefine Isberg <95719327+sojse@users.noreply.github.com>
2023-01-04 15:09:27 +0100

    Set up the playfield
    Generating the squares for the moles and giving both the squares and moles individual IDs

--- src/app/play-field/play-field.component.ts
+++ src/app/play-field/play-field.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./play-field.component.css']
 })
 export class PlayFieldComponent {
+  amountOfSquares: number[] = [1, 2, 3, 4, 5];
 
+  //method used to generate the playfield array in the html
+  counter( i: number ) {
+    return new Array(i);
+  }
+ 
 }
